d32b0f1e3b3b4102f9a88217d6ba6bbc4689a3f8
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2020-05-27 13:20:39 +0200

    Improve tracking and early stopping

--- convnets.py
+++ convnets.py
@@ -2,6 +2,7 @@ import copy
 
 import click
 import h5py
+from sklearn import metrics
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader, TensorDataset
@@ -228,64 +229,55 @@ def init_weights(m):
 
 
 def get_convnet(convnet):
-    if convnet == "VGG Net-A":
+    if convnet == "VGG-Net-A":
         return vgg_net_a()
-    if convnet == "VGG Net-B":
+    if convnet == "VGG-Net-B":
         return vgg_net_b()
-    if convnet == "VGG Net-D":
+    if convnet == "VGG-Net-D":
         return vgg_net_d()
-    if convnet == "VGG Net-E":
+    if convnet == "VGG-Net-E":
         return vgg_net_e()
-    if convnet == "VGG Net-0":
+    if convnet == "VGG-Net-0":
         return vgg_net_0()
-    if convnet == "VGG Net-1":
+    if convnet == "VGG-Net-1":
         return vgg_net_1()
-    if convnet == "VGG Net-2":
+    if convnet == "VGG-Net-2":
         return vgg_net_2()
-    if convnet == "VGG Net-3":
+    if convnet == "VGG-Net-3":
         return vgg_net_3()
-    if convnet == "VGG Net-4":
+    if convnet == "VGG-Net-4":
         return vgg_net_4()
-    if convnet == "VGG Net-5":
+    if convnet == "VGG-Net-5":
         return vgg_net_5()
 
 
-def load_data():
+def load_datasets():
     with h5py.File("sdss_dr14.hdf5", "r") as datafile:
         grp = datafile['2048_zwarning==0']
-        X = grp["X_tr"][:]
-        y = grp["y_tr"][:]
-        X_va = grp["X_va"][:]
-        y_va = grp["y_va"][:]
-
-    return {"ds": TensorDataset(*list(map(torch.from_numpy, [X.reshape(-1, 1, N_WAVES), y.astype("f4")]))),
-            "X_va": torch.from_numpy(X_va.reshape(-1, 1, N_WAVES)),
-            "y_va": torch.from_numpy(y_va.astype("f4"))}
+        X = grp["X_tr"][:].reshape(-1, 1, N_WAVES)
+        y = grp["y_tr"][:].astype("f4")
+        X_va = grp["X_va"][:].reshape(-1, 1, N_WAVES)
+        y_va = grp["y_va"][:].astype("f4")
+    return TensorDataset(*list(map(torch.from_numpy, [X, y]))), TensorDataset(*list(map(torch.from_numpy, [X_va, y_va])))
 
 
-def evaluate(convnet, ds):
-    dev = torch.device("cuda")
-    dl = DataLoader(ds, batch_size=2 ** 15)
-    criterion_sum = nn.BCEWithLogitsLoss(reduction="sum")
-    loss_tr = 0
-    for xb, yb in dl:
-        xb, yb = xb.to(dev), yb.to(dev)
-        loss_tr += criterion_sum(convnet(xb), yb.unsqueeze(-1)).item()
-    return loss_tr / ds.tensors[0].shape[0]
-
-
-def train(convnet, data_dict, patiance, n_validation, n_training, writer):
+def train(convnet, datasets, writer, n_validation):
     dev = torch.device("cuda")
     convnet = convnet.to(dev)
 
-    ds, X_va, y_va = data_dict["ds"], data_dict["X_va"].to(dev), data_dict["y_va"].to(dev)
-    dl = DataLoader(ds, batch_size=256, shuffle=True)
+    dl = DataLoader(datasets[0], batch_size=256, shuffle=True)
+    X_va = datasets[1].tensors[0].to(dev)
+    y_va_dev = datasets[1].tensors[1].to(dev).unsqueeze(-1)
+    y_va_cpu = datasets[1].tensors[1]
 
     opt = optim.Adam(convnet.parameters())
     criterion = nn.BCEWithLogitsLoss()
 
-    best_va_loss, j, i = float("inf"), 0, 0
-    while j < patiance:
+    i = 0
+    best_f1_score = float("inf")
+    improvement = True
+    while improvement:
+        improvement = False
         for xb, yb in dl:
             convnet.train()
             xb, yb = xb.to(dev), yb.to(dev)
@@ -293,31 +285,22 @@ def train(convnet, data_dict, patiance, n_validation, n_training, writer):
             loss = criterion(convnet(xb), yb.unsqueeze(-1))
             loss.backward()
             opt.step()
+            writer.add_scalar("Loss/training", loss.item(), i + 1)
 
             convnet.eval()
             with torch.no_grad():
-                # validation loss
+                # validation loss and early stopping
                 if i % n_validation == 0:
-                    loss_va = criterion(convnet(X_va), y_va.unsqueeze(-1)).item()
-                    writer.add_scalars("Loss", {"Validation set loss": loss_va}, i + 1)
+                    outputs = convnet(X_va)
+                    loss_va = criterion(outputs, y_va_dev)
+                    f1_score = metrics.f1_score(y_va_cpu, torch.sigmoid(outputs).cpu() > 0.5)
+                    writer.add_scalar("Loss/validation", loss_va.item(), i + 1)
+                    writer.add_scalar("F1 score/validation", f1_score, i + 1)
 
-                    if loss_va < best_va_loss:
-                        j = 0
+                    if f1_score < best_f1_score:
+                        best_f1_score = f1_score
                         best_state_dict = copy.deepcopy(convnet.state_dict())
-                        best_va_loss = loss_va
-                    else:
-                        j += 1
-
-                    if not j < patiance:
-                        # last training loss evaluation
-                        loss_tr = evaluate(convnet, ds)
-                        writer.add_scalars("Loss", {"Training set loss": loss_tr}, i + 1)
-                        break
-
-                # training loss
-                if i % n_training == 0:
-                    loss_tr = evaluate(convnet, ds)
-                    writer.add_scalars("Loss", {"Training set loss": loss_tr}, i + 1)
+                        improvement = True
 
             # next iteration
             i += 1
@@ -325,37 +308,19 @@ def train(convnet, data_dict, patiance, n_validation, n_training, writer):
     return best_state_dict
 
 
-CONVNETS = ["VGG Net-A",
-            "VGG Net-B",
-            "VGG Net-D",
-            "VGG Net-E",
-            "VGG Net-0",
-            "VGG Net-1",
-            "VGG Net-2",
-            "VGG Net-3",
-            "VGG Net-4",
-            "VGG Net-5"]
+CONVNETS = ["VGG-Net-A", "VGG-Net-B", "VGG-Net-D", "VGG-Net-E", "VGG-Net-0",
+            "VGG-Net-1", "VGG-Net-2", "VGG-Net-3", "VGG-Net-4", "VGG-Net-5"]
 
 @click.command()
-@click.option("--convnet",
-              type=click.Choice(CONVNETS))
-@click.option("-p", "--patiance", default=1024, show_default=True,
-              help="The number of times to observe worsenning validation set error before giving up.")
-@click.option("--n-validation", default=1, show_default=True,
-              help="The number of steps between validation loss evaluations.")
-@click.option("--n-training", default=256, show_default=True,
-              help="The number of steps between training loss evaluations.")
-@click.argument("save_path", type=click.Path(dir_okay=False, writable=True))
-def cli(convnet, patiance, n_validation, n_training, save_path):
+@click.option("--convnet", type=click.Choice(CONVNETS))
+@click.option("--n-validation", default=1, show_default=True, help="The number of steps between validation loss evaluations.")
+def cli(convnet, n_validation, save_path):
     writer = SummaryWriter(comment="_{}".format(convnet))
-    click.echo("Loading data...")
-    data_dict = load_data()
+    datasets = load_datasets()
     convnet = get_convnet(convnet)
     convnet.apply(init_weights)
-    click.echo("Training...")
-    best_state_dict = train(convnet, data_dict, patiance, n_validation, n_training, writer)
-    click.echo("Saving...")
-    torch.save(best_state_dict, save_path)
+    best_state_dict = train(convnet, datasets, writer, n_validation)
+    torch.save(best_state_dict, convnet + ".pt")
 
 
 if __name__ == "__main__":
